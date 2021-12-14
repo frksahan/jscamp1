51709829bc8cb79ee661247f521291610f498686
Ömer Faruk <sahan.faruk.omer@outlook.com>
2021-12-14 23:37:23 +0300

    14 Aralık Çalışma

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCurringConcerns/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -13,8 +14,8 @@ let user2 = new User(2, "Ömer Faruk ", "ŞAHAN", "GİRESUN")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
 
 
@@ -23,3 +24,17 @@ let customer = {id:1, firstName: "Engin"}
 customer.lastName = "Demiroğ"
 
 console.log(customer.lastName)
+
+console.log("-------------------------")
+userService.load()
+
+
+let customerToAdd = new Customer(1, "Seda", "YILMAZ", "Ankara", "fxfbxfbx" )
+customerToAdd.type = "customer"
+
+userService.add( customerToAdd)
+ console.log(userService.customers)
+ console.log(userService.employees)
+ console.log(userService.errors)
+ console.log(userService.getCustomersSorted())
+
---
+++ src/data/users.js
@@ -0,0 +1,57 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Ömer Faruk",
+        lastName: "ŞAHAN",
+        age: 22,
+        city : "Adana",
+        creditCardNumber: "772807",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Zehra",
+        lastName: "ALEŞ",
+        city: "Samsun",
+        age: "27",
+        creditCardNumber: "959911",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Muhammed",
+        lastName: "SARAÇ",
+        city: "Bursa",
+        age: "24",
+        salary: 8000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Yavuz",
+        lastName: "SELİM",
+        city: "Şırnak",
+        age: 10,
+        salary: 9500,
+        type: "employee"
+    }
+    ,
+    {
+        id: 5,
+        firstName: "Ahmet",
+        lastName: "BİLAL",
+        city: "Ağrı",
+        age: 11,
+        salary: 9000,
+        type: "employee"
+    },
+    {
+        id: 6,
+        firstName: "Sedat",
+        lastName: "HARUN",
+        age: 22,
+        city : "Bremen",
+        creditCardNumber: "72828287",
+        type: "customer"
+    }
+]
